44981f2c23eef2ce2bdba3e1f9915cf60fd93d8d
abdelmajied94abusuliman <115414116+abdelmajied94abusuliman@users.noreply.github.com>
2022-12-12 16:40:01 +0300

    PHP TASKS

---
+++ testForSession.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+echo $_SESSION['sampleFile'];
+echo '<br>';
+echo $_SESSION['lastModified'] ;
+echo '<br>';
+echo $_SESSION['currentFile'] ;
+echo '<br>';
+echo $_SESSION['numLines'] ;
+echo '<br>';
+echo $_SESSION['table'];
+echo '<br>';
